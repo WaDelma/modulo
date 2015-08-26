fad159e595d8d84c64c387a63e009c48573b3a59
Wadelma <Antti@MahComputer.(none)>
2015-08-26 22:40:57 +0300

    Added documentation and tests.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,14 @@
 use std::ops::{Rem, Add};
-
+/// Trait that describes modulo operation
+/// # Examples
+///
+/// ```¨
+/// use modulo::Mod;
+/// fn main() {
+///     assert_eq!(4, (-2).modulo(6));
+///     assert_eq!(2, 9.modulo(7));
+/// }
+/// ```
 pub trait Mod<RHS=Self> {
     type Output;
     fn modulo(self, rhs: RHS) -> Self::Output;
@@ -14,5 +23,33 @@ impl<A, B, C> Mod<B> for A where A: Rem<B, Output=C>, B: Clone, C: Add<B, Output
         } else {
             result
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::Mod;
+
+    #[test]
+    fn smaller_than_modulus_works() {
+        assert_eq!(3, 3.modulo(9));
+        assert_eq!(7, 7.modulo(8));
+    }
+
+    #[test]
+    fn larger_than_modulus_works() {
+        assert_eq!(1, 6.modulo(5));
+        assert_eq!(8, 18.modulo(10));
+    }
+
+    #[test]
+    fn megative_works() {
+        assert_eq!(1, (-1).modulo(2));
+        assert_eq!(2, (-7).modulo(3));
+    }
+
+    #[test]
+    fn big_number_works() {
+        assert_eq!(1, 101.modulo(10));
     }
 }
